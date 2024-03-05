8c2313d322fd87203bdb493ac9c1020266a4285b
Diego Gagliardi <diegotgagliardi@gmail.com>
2024-03-05 19:38:35 -0300

    Aula 2 PHP

---
+++ Aula 1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lembrando do HTML</title>
+</head>
+<body>
+    <nav>
+        <div class="menu">
+            <a href="index.html">Home</a>
+            <a href="contato.html">Contato</a>
+            <a href="tabela.html">Tabela</a>
+        </div>
+    </nav>
+    </nav>
+    <h1>AULA PHPH - LEMBRANDO DO HTML</h1>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro praesentium, ducimus optio dignissimos eos quaerat possimus rerum voluptatum maiores earum. At aut voluptatibus cupiditate, saepe atque natus porro. Illo, inventore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sit porro consectetur possimus iusto libero cum voluptas suscipit harum necessitatibus placeat aut, totam voluptatem itaque, vel labore, cumque consequatur doloribus.</p>
+
+    <?php 
+        $nomeVariavel = 10;
+        echo "Olá Mundo!" . $nomeVariavel
+    
+    ?>
+</body>
+</html>
---
+++ Aula 2/Aula2.php
@@ -0,0 +1,26 @@
+<?php
+    //Comentario em PHP
+    echo "Ola Mundo!"; //Escreve qualquer coisa e printa
+    
+    echo "<div>";
+
+    echo "<h1> Ola Mundo</>";
+
+    echo "</div>";
+
+    $numero = 3;
+    // $numero = "textinho legal";
+
+    echo $numero;
+
+    $nome = "Leopoudo";
+    $idade = 56;
+    $faculdade = true;
+
+    echo "<h2> Pessoa: </h2>";
+    echo "Nome: " . $nome . "<br>";
+    echo "<br>";
+    echo "Idade: " . $idade . "<br>";
+    echo "Faculdade: " . $faculdade . "<br>";
+
+?>
